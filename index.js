fa5dd369181952904e815aac70f4072b307696fb
mattperkins <github@mattperkins.com>
2018-09-08 13:08:21 +0100

    run deployment test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/dev', (req,res) => {
-	res.send('<h1>Hello, World!</h1>')
+	res.send('<h1>Hello, World!!!</h1>')
 })
 
 app.get('/json', (req,res) => {
