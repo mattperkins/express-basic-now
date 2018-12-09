04586f7a60c1544518ca5424cf700a3a626b6553
mattperkins <github@mattperkins.com>
2018-12-09 21:38:24 +0000

    add environment variables

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv')
+  dotenv.config()
+}
 
-app.get('/dev', (req,res) => {
-	res.send('<h1>Development route</h1>')
+app.get('/dev', (req, res) => {
+  res.send('<h1>Development route</h1>')
 })
 
-app.get('/json', (req,res) => {
-    res.json({
-	endpoint: '/json ' + process.env.DATA_DUMP})
+app.get('/json', (req, res) => {
+  res.json({
+    endpoint: '/json ' + process.env.DATA_DUMP })
 })
 
-const PORT = 3000; 
+const PORT = 3000
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
 
